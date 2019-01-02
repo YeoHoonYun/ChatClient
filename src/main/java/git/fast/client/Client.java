3dbac0a6123b59374914df4fd9f625a2ba920f8f
yun <cjswo9207@gmail.com>
2019-01-02 18:56:07 +0900

    client

--- src/main/java/git/fast/client/Client.java
+++ src/main/java/git/fast/client/Client.java
@@ -1,6 +1,7 @@
 package git.fast.client;
 
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
@@ -8,25 +9,31 @@ import java.net.Socket;
  * Github : https://github.com/YeoHoonYun
  */
 public class Client {
-    Socket socket = null;
+    private String ip;
+    private int port;
 
-    public Client(Socket socket) {
-        this.socket = socket;
+    public Client(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
     }
 
     public void connect(){
+        Socket socket = null;
         DataOutputStream out = null;
         BufferedReader br= null;
 
         try {
+            socket = new Socket(ip,port);
             out = new DataOutputStream(socket.getOutputStream());
-            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            br = new BufferedReader(new InputStreamReader(System.in));
 
             ClientHandler clientHandler = new ClientHandler(socket);
             clientHandler.start();
 
             while(true){
+                System.out.print("text를 입력하세요. : ");
                 String text = br.readLine();
+
                 out.writeUTF(text);
                 out.flush();
             }
@@ -48,8 +55,8 @@ public class Client {
     }
 
     public static void main(String[] args) {
-
-
+        Client client = new Client("localhost" , 8000);
+        client.connect();
     }
 
 }
