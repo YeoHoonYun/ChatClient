fa352937a8a5d61bf9d2ce6a13ea80b8029b9167
yun <cjswo9207@gmail.com>
2019-01-04 02:01:39 +0900

    no message

--- src/main/java/git/fast/client/ClientHandler.java
+++ src/main/java/git/fast/client/ClientHandler.java
@@ -23,21 +23,25 @@ public class ClientHandler extends Thread {
             while (true) {
                 String string = in.readUTF();
                 if(string.split("///")[0].equals("400")){
-                    System.out.println("성공적으로 서버에 나갔습니다.");
+                    System.out.println("* 서버와 연결을 끊습니다.");
                     System.out.println("채팅 프로그램을 종료합니다.");
                     System.exit(0);
                 }
                 else if(string.split("///")[0].equals("100")){
+                    System.out.println("* 닉네임 정보가 설정되었습니다.");
                     System.out.println(string.split("///")[1]);
                 }
                 else if(string.split("///")[0].equals("200")){
+                    System.out.println("* 유저로부터의 메시지입니다.");
                     System.out.println(string.split("///")[1]);
                 }
                 else if(string.split("///")[0].equals("300")){
+                    System.out.println("* 서버로부터의 메시지입니다.");
                     System.out.println(string.split("///")[1]);
                 }
-                else
+                else{
                     System.out.println(string);
+                }
             }
         } catch (IOException e) {
             System.out.println("접속을 종료합니다.");
